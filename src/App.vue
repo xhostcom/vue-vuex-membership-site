0594669adb4569d038f877c43c7dda331e22df59
Paul Anthony McGowan <xhostcom@gmail.com>
2020-03-02 22:43:34 +0000

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="app">
+<Navbar />
+<router-view />
+</div>
+</template>
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+components: {
+Navbar
+}
+}
+</script>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/login",
+    name: "Login",
+    // eslint-disable-next-line prettier/prettier
+    component: () => import( "@/views/Login.vue")
+  },
+  {
+    path: "/register",
+    name: "Register",
+    // eslint-disable-next-line prettier/prettier
+    component: () => import( "@/views/Login.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
